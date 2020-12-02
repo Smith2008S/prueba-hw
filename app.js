489c99a93eec18a8004d70975f6ff6b044931f3e
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-12-02 10:37:16 -0500

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get('/2.0', function (req, res) {
   res.send('Hello World 2.0!\n');
 });
 
+app.get('/3.0', function (req, res) {
+  res.send('Hello World 3.0!\n');
+});
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
